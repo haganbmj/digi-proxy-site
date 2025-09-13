d8f5c08fd31eac8028b3ee82fe6131861b21eaa6
Brendan Hagan <bhaganksu@gmail.com>
2025-09-13 10:19:18 -0400

    fix: update help modal to reflect digimon

--- src/components/HelpModal.vue
+++ src/components/HelpModal.vue
@@ -16,31 +16,21 @@
           <p>Once your list is loaded, you can adjust set selections using the dropdowns below each card.</p>
           <p>Either click the <i>Print</i> button on the page or use your browser's Print menus to complete the process.</p>
 
-          <h5>Tokens</h5>
-          <hr/>
-          <p>Tokens are tricky to support, so they're all just keyed by their Scryfall name (ex. <kbd>Goblin</kbd>, <kbd>Spirit</kbd>, <kbd>Beast</kbd>).</p>
-          <p>Different variations of tokens that use the same name will all be comingled and you'll need to specify a printing.</p>
-          <p>In the event of tokens that share a name with a card the default selection will use the card (ex. <kbd>Thundertrap Trainer</kbd>).</p>
-
           <h5>Exact Matches</h5>
           <hr/>
-          <p>By default the oldest standard printing of a card is used.</p>
-          <p>If you would like to specify the printing enable <b>Match Input Editions</b> in the configuration and use Arena decklist syntax (ex. <kbd>Thundertrap Trainer (TBLB) 9</kbd>).</p>
+          <p>By default the first alphabetical printing of a card is used?</p>
+          <p>If you would like to specify the printing enable <b>Match Input Editions</b> in the configuration and use DigimonCard.io decklist syntax (ex. <kbd>Agumon BT1-010</kbd>).</p>
 
           <h5>Print Formatting</h5>
           <hr/>
           <p><b>Paper Size:</b> Use your browser's print dialog to configure options such as the paper size and printer destination. Saving as a PDF is listed as a printer selection in all modern browsers.</p>
           <p><b>Margins:</b> I recommend <b>not</b> adjusting the margins on the page, but wish you the best of luck if you opt to.</p>
 
-          <h5>Misc</h5>
-          <hr/>
-          <p>This site is modeled as a substitute for MTGPress since that site has been unusable for a while. As such it's using the cropped down images, so expect thin borders when using it. I personally prefer this, but some people find find it more tedious to cut out.</p>
-
           <h5>Build Details</h5>
           <hr/>
           <ul>
-            <li>Repository: <a href="https://github.com/haganbmj/mtg-proxy-site" target="_blank">github.com/haganbmj/mtg-proxy-site</a></li>
-            <li>Build SHA: <a :href="'https://github.com/haganbmj/mtg-proxy-site/commit/' + getBuildSha()" target="_blank">{{ getBuildSha() }}</a></li>
+            <li>Repository: <a href="https://github.com/haganbmj/digi-proxy-site" target="_blank">github.com/haganbmj/digi-proxy-site</a></li>
+            <li>Build SHA: <a :href="'https://github.com/haganbmj/digi-proxy-site/commit/' + getBuildSha()" target="_blank">{{ getBuildSha() }}</a></li>
             <li>Timestamp: {{ getBuildTimestamp() }}</li>
           </ul>
         </div>
